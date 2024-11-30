ec73caca1cfb75db4578ccff542f9b8dd83d1bfa
16IMA <imonfort001@gmail.com>
2024-11-30 15:30:07 +0100

    ejercicio calculadora

--- ejercicio-calculadora/main.js
+++ ejercicio-calculadora/main.js
@@ -1,2 +1,16 @@
 'use strict'
 
+const calculadoraDeCositas = (boderbox, width, padding, bodersize) => {
+    if (boderbox === true) {
+        console.log(width + padding + bodersize);
+    }
+    
+    else {
+        console.log(width + padding);
+    }
+
+    
+}
+
+const resultado = calculadoraDeCositas(true, 100, 20, 5)
+
