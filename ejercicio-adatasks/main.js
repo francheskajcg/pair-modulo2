51ca317e7cd3440edc7f24a54b3091bafeb1c84f
Francheska Contreras Garcia <frankeka.jose@gmail.com>
2024-12-10 21:35:41 +0100

    add adatasks

---
+++ ejercicio-adatasks/main.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const add = document.querySelector ('.js_add');
+const btnAdd = document.querySelector ('.btn-add');
+const listTask = document.querySelector ('.task-ul');
+const check = document.querySelector ('.checkbox');
+
+btnAdd.addEventListener ('click', (ev) => {
+    ev.preventDefault();
+    listTask.innerHTML += `<li><input class="checkbox" type="checkbox" id="checkBox">` + ' ' + `${add.value}</li>`;
+    add.value = " ";
+console.log(add.value);
+});
+
+check.addEventListener ('click', (ev) => {
+
+});
