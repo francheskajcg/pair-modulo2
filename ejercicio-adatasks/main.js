a63860de94ddf1712f64038b67a7a50d03c10328
martaxxa <mchacartegui29@gmail.com>
2024-12-14 13:59:13 +0100

    Add changes sabado

--- ejercicio-adatasks/main.js
+++ ejercicio-adatasks/main.js
@@ -5,35 +5,41 @@ const btnAdd = document.querySelector ('.btn-add');
 const listTask = document.querySelector ('.task-ul');
 const check = document.querySelector ('.checkbox');
 
-const tasks = [
-    { name: "Recoger setas en el campo", completed: true, id: 1 },
-    { name: "Comprar pilas", completed: true, id: 2 },
-    { name: "Poner una lavadora de blancos", completed: true, id: 3 },
-    {
-      name: "Aprender cómo se realizan las peticiones al servidor en JavaScript",
-      completed: false,
-      id: 4,
-    },
-  ];
+// const GITHUB_USER = martaxxa;
+// const SERVER_URL = `https://dev.adalab.es/api/todo/${GITHUB_USER}`;
 
-  
-/*btnAdd.addEventListener ('click', (ev) => {
-    
-    ev.preventDefault();
-    listTask.innerHTML += `<li><input class="checkbox" type="checkbox" id="checkBox">` + ' ' + `${add.value}</li>`;
-    add.value = " ";
-    
-});
-check.addEventListener ('click', (ev) => {
+let tasks = [];
 
+fetch('https://dev.adalab.es/api/todo/martaxxa')
+    .then((response) => response.json())
+    .then((data) => {
+        tasks = data;
+        let html = '';
+        for (const addObj of tasks) {
+            if(addObj.completed === true){  
+                html += `<li><input class="checkbox"  type="checkbox" id="checkBox" checked>` + ' ' + `${addObj.name}</li>`;
+            }
+            else{
+                html += `<li><input class="checkbox"  type="checkbox" id="checkBox">` + ' ' + `${addObj.name}</li>`;
+            }
+        }
+        listTask.innerHTML = html;
+    });
+
+
+const UlInner = (oneObject) => {
+    const html = listTask.innerHTML += `<li><input class="checkbox"  type="checkbox" id="checkBox">` + ' ' + `${oneObject.name}</li>`;
+    return html;
+}
+
+/*check.addEventListener ('click', (ev) => {
 });*/
 
-for (const addObj of tasks) {
+
+btnAdd.addEventListener ('click', (ev) => {
     
-    if(addObj.completed === true){  
-        listTask.innerHTML += `<li><input class="checkbox"  type="checkbox" id="checkBox" checked>` + ' ' + `${addObj.name}</li>`;
-    }
-    else{
-        listTask.innerHTML += `<li><input class="checkbox"  type="checkbox" id="checkBox">` + ' ' + `${addObj.name}</li>`;
-    }
-}
+    ev.preventDefault();
+    UlInner(add.name);
+    
+});
+
